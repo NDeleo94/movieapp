8ceb5a2d6d4b75a611d287dfd57d540a8d131bc8
NDeleo94 <n.deleo94@gmail.com>
2022-08-06 12:35:00 -0300

    fix url getPoster

--- src/utils/getPoster.js
+++ src/utils/getPoster.js
@@ -2,6 +2,7 @@ import unavailable from "../images/unavailable.jpeg";
 
 export function getPoster(path) {
   const API = "http://127.0.0.1:8000/storage/images/";
+  // const API = "https://still-wildwood-13855.herokuapp.com/storage/images/";
   if (path === "") {
     return unavailable;
   }
